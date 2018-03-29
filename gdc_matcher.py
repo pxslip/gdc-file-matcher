09a2948c02d42edd24ff7dc93c88d44297494aa7
Will Kruse <wskruse@gmail.com>
2018-03-28 22:23:02 -0400

    Add a `--files-file` option for passing in a text file of filenames to match

--- gdc_matcher.py
+++ gdc_matcher.py
@@ -6,6 +6,7 @@ import json
 import os
 import requests
 import time
+import re
 
 FILES_ENDPOINT = 'https://api.gdc.cancer.gov/files'
 DESC = """This program will attempt to use the GDC api to associate a file or set of files with any fields specified
@@ -45,12 +46,14 @@ parser.add_argument('--fields',
 parser.add_argument('--files',
                     help='A list of filenames to get the field data for',
                     action='append')
+parser.add_argument('--files-file',
+                    help='A file containing a space, comma or newline delimited list of files to get field data for')
 parser.add_argument('--dir',
                     help='Instead of a list of files, specify a directory with the files to use, these must have the original filename')
 
 args = parser.parse_args()
 
-if not args.files and not args.dir:
+if not args.files and not args.dir and not args.files_file:
     parser.print_help()
 else:
     fields_arg = args.fields
@@ -60,11 +63,14 @@ else:
         fields_arg.extend(['cases.case_id', 'file_name'])
     # Actually do something
     # prefer files over dir
+    files_list = []
     if args.files:
-        associate(args.files, fields_arg)
-    else:
-        files_list = []
+        files_list = args.files
+    elif args.dir:
         for dirpath, dirnames, filenames in os.walk(args.dir):
             for filename in filenames:
                 files_list.append(filename)
-        associate(files_list, fields_arg)
+    elif args.files_file:
+        with open(args.files_file) as input_file:
+            files_list = re.compile("[,\s\n]").split(input_file.read())
+    associate(files_list, fields_arg)
